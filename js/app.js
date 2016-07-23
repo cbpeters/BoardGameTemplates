2a4fc4f7d0ad717280872150a4543e6027ff5589
Cory Peters <cory_peters@shi.com>
2016-07-23 16:39:50 -0400

    added quests

--- js/app.js
+++ js/app.js
@@ -2,10 +2,14 @@ var gameApp = angular.module('gameApp', []);
 gameApp.directive('cards', function () {
     return {        
         controller: function($scope, $http) {    
-            $http.get('/cards.json')
-                .then(function(result){
-                    $scope.cards = result.data;                
-                });
+            $http.get('/cards.json').then(function(result){
+                $scope.cards = result.data;                
+            });
+
+            $http.get('/quests.json').then(function(result){
+                $scope.quests = result.data;                
+            });
+            
         }
     };
 });
